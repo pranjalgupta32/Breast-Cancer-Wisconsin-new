a80bbb096f80a6170888b19b8aa5c7a5e030d636
Pranjal Gupta <107236387+pranjalgupta32@users.noreply.github.com>
2023-06-14 22:22:15 -0400

    BreastCancerModelV1.py

--- BreastCancer.py
+++ BreastCancer.py
@@ -1,1 +1,31 @@
-abcd
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+# Path to the dataset file
+
+data_file = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+
+# Load the dataset
+data = pd.read_csv(data_file, header=None)
+
+# Extract features (X) and labels (y)
+X = data.iloc[:, 2:].values
+y = data.iloc[:, 1].map({'M': 1, 'B': 0}).values
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Initialize the Random Forest Classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model
+model.fit(X_train, y_train)
+# Make predictions on the testing set
+y_pred = model.predict(X_test)
+
+# Calculate the accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+Accuracy: 0.9649122807017544
+ 
